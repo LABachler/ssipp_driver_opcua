9082e4975c0f56101128a0ecd9467ec653c31a76
Lukas Andrea Bachler <lukas.bachler@gmail.com>
2022-01-10 18:13:21 +0100

    Redis Connection not working

--- Redis/RedisConnector.ts
+++ Redis/RedisConnector.ts
@@ -1,19 +1,48 @@
 class RedisConnector{
     static PORT = 6379;
-    static HOST = "localhost";
+    static HOST = "127.0.0.1";
     static IP_PROTOCOL = 4;
     static PASSWORD = '';
     static DB = 0;
+    private _redisString: string;
+
+    get redisString(): string {
+        return this._redisString;
+    }
+
+    static renewRedisString(processId: number) {
+        const redis = require("redis");
+        const client = redis.createClient("memurai://127.0.0.1:6379");
+
+        client.on('connect', function() {
+            console.log('Connected!');
+        });
+
+        client.lRange("ssipp_process_data", processId, processId).then(function (result) {
+            console.log('Promise');
+            this._redisString = result;
+        });
+    }
+
+    static getRedisClient() {
+        const Redis = require("redis");
+        const client = Redis.createClient();
+        client.on('connect', function() {
+            console.log('Connected!');
+        });
+        return client;
+    }
 
     static getRedisInstance(){
         const Redis = require("ioredis");
-        return new Redis({
+        return new Redis();
+        /*return new Redis({
             port: RedisConnector.PORT,
             host: RedisConnector.HOST,
             family: RedisConnector.IP_PROTOCOL,
             password: RedisConnector.PASSWORD,
             db: RedisConnector.DB
-        });
+        });*/
     }
 }
 
--- XML/XMLHandler.ts
+++ XML/XMLHandler.ts
@@ -15,21 +15,18 @@ class XMLHandler {
     }
 
     constructor() {
-        console.log("XML Constructor called");
         if (process.argv[2] === undefined)
             this.processId = 0; //0 is DemoData
         else
             this.processId = parseInt(process.argv[2]);
-        this._redisConnection = MyRedisConnector.RedisConnector.getRedisInstance();
+        this._redisConnection = MyRedisConnector.RedisConnector.getRedisClient();
         this.renewDocFromRedis();
         this._xPath = require("xpath-ts"); //TODO
         this._running = true;
     }
 
     renewDocFromRedis() {
-        this._doc = new MyDOMParser.DOMParser().parseFromString(
-            this._redisConnection.zrange("ssipp_process_data", this.processId, this.processId), "text/xml");
-        console.log(this._doc.toString());
+        MyRedisConnector.RedisConnector.renewRedisString(this.processId);
     }
 }
 
